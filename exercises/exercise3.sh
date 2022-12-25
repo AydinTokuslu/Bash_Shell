7abb890eae1f966765f9650fe3dea2308d43ce2f
aydin <aydintokuslu78@gmail.com>
2022-12-25 16:51:05 +0300

    25.12.22

---
+++ exercises/exercise3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kelimeler="insan kedi kitap python koyun php deve perl ruby"
+for kelime in $kelimeler
+do
+  echo $kelime 
+done
--- meta_karakterler.sh
+++ meta_karakterler.sh
@@ -41,7 +41,9 @@
 # echo {01..10} #01den 10a kadar yazdırır
 # # 01 02 03 04 05 06 07 08 09 10
 
-echo {a,b,c}{1..5}
+#echo {a,b,c}{1..5}
 #a1 a2 a3 a4 a5 b1 b2 b3 b4 b5 c1 c2 c3 c4 c5
+
+echo "pid $$ dir"
 
 
