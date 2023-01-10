a51270874a21e3d7069e8d2c9570700cfaf3d5a1
aydin <aydintokuslu78@gmail.com>
2023-01-10 17:36:15 +0300

    10.01.23

--- real-life-scripting/delete-old-files.sh
+++ real-life-scripting/delete-old-files.sh
@@ -5,3 +5,7 @@ find /home/aydintokus/old-files -mtime +150 -exec ls -l {} \;
 
 find /home/aydintokus/old-files -mtime +150 -exec rm {} \;
 #150 günden daha az zamandaki eski dosyaları sil
+
+#Find and rename old files
+
+find . -mtime +90 -exec mv {} {}.old \;
