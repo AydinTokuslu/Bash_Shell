8b94811331780e40e724fc627d040ff5615bf4b3
aydin <aydintokuslu78@gmail.com>
2023-01-20 00:08:05 +0300

    20.01.23

--- projects/project101/part4/Task-3/invalid_userr.sh
+++ projects/project101/part4/Task-3/invalid_userr.sh
@@ -13,3 +13,5 @@
 #awk '/Invalid user/' auth.log | awk -F"]: " '{print $2}' | awk -F" " '{print $3}' | sort | uniq -c > invalid_user.sh
 # or
 #cat auth.log | grep "Failed password for invalid user" | awk '{print $11}' | sort | uniq -c > result.txt
+
+grep "invalid user" auth.log | awk '{print $11}' | sort | uniq -c > invalid_user.sh
