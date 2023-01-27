da03aa991b1d76d7ef56a9aeab97084445f08d4b
aydin <aydintokuslu78@gmail.com>
2023-01-27 10:22:04 +0300

    27.01.23

--- python/generate_password_deneme.py
+++ python/generate_password_deneme.py
@@ -1,27 +1,29 @@
-#Write a Python program that prompts the user to enter his/her full name 
-# (without any spaces) and 
-# then creates a secret password consisting of three letters (in lowercase) 
-# randomly picked up from his/her full name, and a random four-digit number. 
-# For example, if the user enters "JackClarusway", 
+# Write a Python program that prompts the user to enter his/her full name
+# (without any spaces) and
+# then creates a secret password consisting of three letters (in lowercase)
+# randomly picked up from his/her full name, and a random four-digit number.
+# For example, if the user enters "JackClarusway",
 # a secret password can probably be one of "jcs1578" or "yka8832" or "awu1250".
 from random import *
 
 
 def generate_passwd():
-    name=input("please enter your full name without any spaces: ")
-    if name.count(" ") > 0:
-        print("There is spaces in your full name, please enter it without any spaces!!")
-        
-    else:
-        passwd=""
-            
-        name2=choices(name, k=3)
-        for i in name2:
-            passwd+=i.lower()
-        number=sample(range(1, 10), 4)
-        for i in number:
-            passwd+=str(i)
-        print(passwd)
+    while True:
+        name = input("please enter your full name without any spaces: ")
+        if name.count(" ") > 0:
+            print(
+                "There is space in your full name, please enter it without any spaces!!")
+        else:
+            passwd = ""
+
+            name2 = choices(name, k=3)
+            for i in name2:
+                passwd += i.lower()
+            number = sample(range(1, 10), 4)
+            for i in number:
+                passwd += str(i)
+            print(passwd)
+            break
 
 
 generate_passwd()
