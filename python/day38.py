db72e03c60afc06597bc24f7431e1dcba5c2deeb
aydin <aydintokuslu78@gmail.com>
2023-02-21 21:22:45 +0300

    21.02.23

--- python/day38.py
+++ python/day38.py
@@ -6,9 +6,28 @@
 # of three guesses. When the user guesses right, the code should 
 # declare them a winner. After three wrong guesses, the code 
 # should declare them a loser.
+from random import randint
+
 
 def guess_a_number():
-    pass
+    count=0
+    random_number=randint(1, 11)
+    print(random_number)
+    while count<3:
+        num=int(input("please guess a number between 1-10, you have 3 choice : "))
+        count+=1
+        if num>random_number:
+            print("your guess is too high")
+        elif num<random_number:
+            print("your guess is too low")
+        else:
+            print("congratulation, you won")
+    if count>=3:
+        print("you are a loser")
+    else:
+        print("you are winner")
+        
+        
 
 
 guess_a_number()
