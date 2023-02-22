31663d804ba7612463c06b38e57bb029d1fe1099
aydin <aydintokuslu78@gmail.com>
2023-02-22 20:37:01 +0300

    22.02.23

--- python/day38_extra.py
+++ python/day38_extra.py
@@ -5,7 +5,13 @@
 # sequence. The list above should return:
 # [4, 8, 10, 13, 16, 29, 30]
 
-def missing_numbers():
-    pass
+def missing_numbers(list):
+    missed_numbers=[]
+    for i in range(1,32):
+        if i not in list:
+            missed_numbers.append(i)
+    print(missed_numbers)
 
-missing_numbers()
+
+list = [1, 2, 3, 5, 6, 7, 9, 11, 12, 23, 14, 15, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 31]
+missing_numbers(list)
