6710513a0f94fb64f3f441d3f254f6e1278f9895
aydin <aydintokuslu78@gmail.com>
2023-02-23 21:20:56 +0300

    23.02.23

--- python/day39.py
+++ python/day39.py
@@ -7,14 +7,24 @@
 # function should generate a 5-character long password. If the user 
 # picks strong, generate an 8-character password and if they pick 
 # very strong, generate a 12-character password.
-from random import randint
+import string
 import random
-def generate_password():
-    password=""
-    generate=[0,1,2,3,4,5,6,7,8,9,["A-Z"],["a-z"],"?","!","<",">","*","/","$","+"]
-    passw=input("please enter your password strength (weak, strong, very strong): ").lower()
-    if passw == "weak":
-        print(random.choices(generate, weights = [5, 1, 1], k = 5))
-        
-
-generate_password()
+def password_generator():
+    # string module constants
+    a = string.ascii_letters + \
+    string.digits + string.punctuation
+    password1 =[]
+    length = input("Pick your password length "
+    "a,b or c: \na. weak \nb.strong \nc."
+    "Very strong...: ")
+    if length == 'a':
+        length = 5
+    elif length == 'b':
+        length = 8
+    elif length == 'c':
+        length = 12
+    for i in range(length):
+        password = str(random.choice(a))
+        password1.append(password)
+    return 'You password is', ''.join(password1)
+print(password_generator())
