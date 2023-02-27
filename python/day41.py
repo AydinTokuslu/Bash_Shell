941021301c4cd35e88c7704cfb3b0f6ce3ee9d12
aydin <aydintokuslu78@gmail.com>
2023-02-27 11:07:29 +0300

    27.02.23

--- python/day41.py
+++ python/day41.py
@@ -4,9 +4,15 @@
 # return [‘You’,’have’, ‘no’].
 
 def words_with_vowels(a):
+    vowels = ["a","e","i","o","u"]
+    new_list=[]
     a=a.split()
     for i in a:
-        print(i)
+        for j in vowels:
+                if j in i:
+                    if i not in new_list:
+                        new_list.append(i)
+    print(new_list)
 
 
 a="You have no rhythm"
