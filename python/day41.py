3e9a9cecf3e431868c80fc4ae7361858631b2bbd
aydin <aydintokuslu78@gmail.com>
2023-02-27 10:57:10 +0300

    27.02.23

--- python/day41.py
+++ python/day41.py
@@ -2,3 +2,10 @@
 # takes a string of words and returns a list of only words that have 
 # vowels in them. For example, ‘You have no rhythm’ should 
 # return [‘You’,’have’, ‘no’].
+
+def words_with_vowels():
+    pass
+
+
+
+words_with_vowels()
