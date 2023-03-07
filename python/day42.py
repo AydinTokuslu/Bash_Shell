7657c7d6e0b8f35ea4a7e42fe9c607f615fb68e1
aydin <aydintokuslu78@gmail.com>
2023-03-07 18:25:27 +0300

    07.03.23

--- python/day42.py
+++ python/day42.py
@@ -1,15 +1,14 @@
-# Write a function called spelling_checker. This code asks the 
-# user to input a word and if a user inputs a wrong spelling it 
-# should suggest the correct spelling by asking the user if they 
-# meant to type that word. If the user says no, it should ask the 
-# user to enter the word again. If the user says yes, it should 
-# return the correct word. If the word entered by the user is 
+# Write a function called spelling_checker. This code asks the
+# user to input a word and if a user inputs a wrong spelling it
+# should suggest the correct spelling by asking the user if they
+# meant to type that word. If the user says no, it should ask the
+# user to enter the word again. If the user says yes, it should
+# return the correct word. If the word entered by the user is
 # correctly spelled the function should return the correct word.
 # Use the module textblob.
 
 def spelling_checker():
     word = input("please enter a word : ")
-    if word == "true":
-        print("true")
+
 
 spelling_checker()
