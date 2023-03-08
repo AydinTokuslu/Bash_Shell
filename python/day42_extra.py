ac3518fca4d9caff366773edd9a054921f971954
aydin <aydintokuslu78@gmail.com>
2023-03-09 00:12:05 +0300

    09.03.23

---
+++ python/day42_extra.py
@@ -0,0 +1,6 @@
+# Create a function called alarm that sets an alarm clock for the 
+# user. The function should ask the user to enter time they want 
+# the alarm to go off. The user should enter the hour and 
+# minute. The function should then print out the time when the 
+# alarm will go off. When its alarm time, the code should let off a 
+# sound. Use the winsound module for sound.
