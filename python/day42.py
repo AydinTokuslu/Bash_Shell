b7c98043e07ca94ab140bfc5a25e3e74a20d929c
aydin <aydintokuslu78@gmail.com>
2023-03-08 15:05:35 +0300

    08.03.23

--- python/day42.py
+++ python/day42.py
@@ -7,9 +7,26 @@
 # correctly spelled the function should return the correct word.
 # Use the module textblob.
 
+from textblob import TextBlob
+
 def spelling_checker():
-    word = input("please enter a word : ")
-    
+    while True:
+        word = input('Enter a word : ')
+        # checking if the input word is correct
+        if word != TextBlob(word).correct():
+            # suggesting the correct word to the user
+            question = input(f'Did you mean '
+                            f'{TextBlob(word).correct()}?. '
+                            f'type yes/no :')
+            if question == 'yes':
+                break
+            else:
+                print('Please try again')
+        # if the word entered is correct 
+        # the code breaks and returns word
+        elif word == TextBlob(word).correct():
+            break
+    return f'Your word is, {TextBlob(word).correct()}'
 
+print(spelling_checker())
 
-spelling_checker()
