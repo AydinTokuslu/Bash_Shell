1bbd2fece26c7b7f59e2b80b157ac27dcafd66cf
aydin <aydintokuslu78@gmail.com>
2023-03-12 11:21:32 +0300

    12.03.23

--- python/day42_extra.py
+++ python/day42_extra.py
@@ -4,9 +4,26 @@
 # minute. The function should then print out the time when the 
 # alarm will go off. When its alarm time, the code should let off a 
 # sound. Use the winsound module for sound.
+import winsound
+import datetime
 
-def alarm():
-    time= input("please enter time (hour and minute) you want the alarm to go off : ")
-    print(f"your alarm will go off at {time}")
 
-alarm()
+def set_alarm():
+    # Enter hour
+    hour = input("Please enter hour: ")
+    # Enter minute
+    minute = input("Please enter minute: ")
+    # concatenate hour and minute
+    alarm_time = hour + ':' + minute
+    # let the user know the alarm time set
+    print("You have set alarm for ", alarm_time)
+    while True:
+        now = datetime.datetime.now().strftime("%H:%M")
+        if alarm_time == now:
+            print("Wake up")
+            print("Wake up")
+            print("Wake up")
+            winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+            break
+
+set_alarm()
