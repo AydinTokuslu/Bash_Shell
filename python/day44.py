fa200ef4441f062d98fb3a8dbedc6b72ba07c0b3
AydinTokuslu <aydintokuslu78@gmail.com>
2023-04-29 00:22:57 +0300

    29.04.23

--- python/day44.py
+++ python/day44.py
@@ -38,8 +38,5 @@ def open_emails():
     with open("mail_records.txt", "r", encoding="utf-8") as file:
             records=file.readlines()
             print(records)
-# def open_emails():
-#     with open("mail_records.txt", "r", encoding="utf-8") as file:
-#             records=file.readlines()
-#             print(records)
+
 
